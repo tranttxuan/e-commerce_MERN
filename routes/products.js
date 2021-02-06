9e692dc66d34419b4226f3e54f7df86a0a58223c
tranttxuan <>
2021-02-06 02:03:47 +0100

    search query

--- routes/products.js
+++ routes/products.js
@@ -5,9 +5,43 @@ const router = express.Router();
 
 //Get all products
 router.get("/", (req, res, next) => {
-     Product.find()
-          .then(data => res.status(200).json(data))
+     //query parameters from frontend
+     const name = req.query.name || '';
+     const category = req.query.category || '';
+     const min = req.query.min && Number(req.query.min) !== 0 ? Number(req.query.min) : 0;
+     const max = req.query.max && Number(req.query.max) !== 0 ? Number(req.query.max) : 0;
+     const rating = req.query.rating && Number(req.query.rating) !== 0 ? Number(req.query.rating) : 0;
+
+     //create query to search
+     const categoryFilter = category ? { category } : {};
+     const priceFilter = min && max ? { price: { $gte: Number(min), $lte: Number(max) } } : {};
+     const ratingFilter = rating ? { rating: { $gt: rating } } : {};
+     //Case insensitivity to match upper and lower cases
+     const nameFilter = name ? { name: { $regex: name, $options: 'i' } } : {};
+     const order = req.query.order
+          ? req.query.order === 'lowest'
+               ? { price: 1 }
+               : req.query.order === "highest" ? { price: -1 }
+                    : req.query.order === "newest" ? { _id: -1 }
+                         : { rating: -1 }
+          : { _id: -1 }
+
+     Product.find({ ...categoryFilter, ...priceFilter, ...ratingFilter, ...nameFilter, ...ratingFilter })
+          .sort(order)
+          .then(data => {
+               console.log({ ...categoryFilter, ...priceFilter, ...ratingFilter, ...nameFilter, ...ratingFilter })
+               res.status(200).json(data)})
           .catch(error => res.status(500).json({ message: error }))
+});
+
+//Get list of categories
+router.get("/categories", (req, res, next) => {
+     Product.find().distinct("category")
+          .then(data => res.status(200).json(data))
+          .catch(error =>
+               res.status(500).json({
+                    message: "Failure to fetch data"
+               }))
 });
 
 //Get a specific product
@@ -19,6 +53,9 @@ router.get("/:idProd", (req, res, next) => {
                     message: "Failure to find this product's id in database"
                }))
 })
+
+
+
 
 //Add a new product
 router.post("/", (req, res, next) => {
